e95f46f9c0a005f73343bd72177174308b728a3d
David Miotti <david.miotti@wopata.com>
2017-03-07 15:33:17 +0100

    add string transformation for iOS

--- Sources/Writer.swift
+++ Sources/Writer.swift
@@ -17,11 +17,28 @@ import Foundation
 func writeiOS(_ translations: [Translation], for projectName: String) throws {
     try translations.forEach { loc in
         let fileUrl = try buildFilePath(forProject: projectName, lang: loc.lang)
-
         // Transform our object to ios data
-        let data = loc.translations.map { "\"\($0)\" = \"\($1)\";" }
-            .joined(separator: "\n").data(using: .utf8)
+        let lines = loc.translations.map { key, value in
+            "\"\(key)\" = \"\(sanitize(value))\";"
+        }
+        let content = lines.joined(separator: "\n")
+        let data = content.data(using: .utf8)
         try data?.write(to: fileUrl)
+    }
+}
+
+/// Transform the string into correct parseable iOS format
+///
+/// - Parameter string: The parsed to be formatted
+/// - Returns: The formatted string
+func sanitize(_ string: String) -> String {
+    let replaceTable = [
+        "%s": "%@",
+        "\"": "\\\"",
+        "%newline%": "\\n"
+    ]
+    return replaceTable.reduce(string) {
+        $0.replacingOccurrences(of: $1.key, with: $1.value)
     }
 }
 
