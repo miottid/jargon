5a38bc02180d7e73aaf2828b0e849ae1c56e4153
David Miotti <david@muxumuxu.com>
2017-04-08 11:20:11 +0200

    Update for i18n

--- Sources/i18nWriter.swift
+++ Sources/i18nWriter.swift
@@ -26,7 +26,7 @@ func writei18n(_ translations: [Translation]) throws -> [URL] {
 /// - translation: The translation to be written
 /// - Throws: Most of the time a filesystem permission problem or insufficient disk space
 private func write(translation: Translation) throws -> URL {
-    let fileUrl = buildFilePath(for: translation)
+    let fileUrl = try buildFilePath(for: translation)
     let contents = try fileContents(for: translation)
     let data = contents.data(using: .utf8)
     try data?.write(to: fileUrl)
@@ -47,23 +47,40 @@ private func fileContents(for translation: Translation) throws -> String {
 /// - Parameter translation: The translation to be converted
 /// - Returns: A dictionnary containing the translation on a YAML format
 private func convertToYAMLDictionnary(_ translation: Translation) -> [String: Any] {
-	let dict = [String: Any]()
+	let dict = NSMutableDictionary()
 	for (tradKey, tradValue) in translation.translations {
         let allKeys = tradKey.components(separatedBy: ".")
+        guard let keyName = allKeys.last else {
+            continue
+        }
+        
         let spaces = allKeys.dropLast()
-        let keyName = allKeys.last
-        print("key: \(keyName), in spaces: \(spaces), for translation: '\(tradValue)'")
-	}
-    return dict
+        var currentDict: NSMutableDictionary? = dict
+        for space in spaces {
+            if currentDict?[space] == nil {
+                let nextDict = NSMutableDictionary()
+                currentDict?[space] = nextDict
+                currentDict = nextDict
+            } else {
+                currentDict = currentDict?[space] as? NSMutableDictionary
+            }
+        }
+        if let currentDict = currentDict {
+            currentDict[keyName] = tradValue
+        }
+    }
+    print(dict)
+    return [:]
 }
 
 /// Transform a Translation object to a string content
 ///
 /// - Parameter translation: The translation to be transformed
 /// - Returns: The string containing the translation text
-func buildFilePath(for translation: Translation) -> URL {
+func buildFilePath(for translation: Translation) throws -> URL {
     let fileManager = FileManager.default
     let currDir = URL(fileURLWithPath: fileManager.currentDirectoryPath)
     let localesDir = currDir.appendingPathComponent("config/locales", isDirectory: true)
+    try fileManager.createDirectory(at: localesDir, withIntermediateDirectories: true, attributes: nil)
     return localesDir.appendingPathComponent("\(translation.lang).yml")
 }
